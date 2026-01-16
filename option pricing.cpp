360ca1e8a8cc91e8660b5e7d2844a472fe8c00fa
Léo Fabrègues <jncsdnvwsj@privaterelay.appleid.com>
2026-01-16 20:20:29 +0100

    c++

--- option pricing.cpp
+++ option pricing.cpp
@@ -1,91 +1,68 @@
-#include <cmath>
+#define _USE_MATH_DEFINES
+
 #include <iostream>
+#include <cmath>
 
-// A simple implementation of the Box-Muller algorithm, used to generate
-// gaussian random numbers - necessary for the Monte Carlo method below
-// Note that C++11 actually provides std::normal_distribution<> in 
-// the <random> library, which can be used instead of this function
-double gaussian_box_muller() {
-  double x = 0.0;
-  double y = 0.0;
-  double euclid_sq = 0.0;
-
-  // Continue generating two uniform random variables
-  // until the square of their "euclidean distance" 
-  // is less than unity
-  do {
-    x = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
-    y = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
-    euclid_sq = x*x + y*y;
-  } while (euclid_sq >= 1.0);
-
-  return x*sqrt(-2*log(euclid_sq)/euclid_sq);
+// Standard normal probability density function
+double norm_pdf(const double& x) {
+    return (1.0/(pow(2*M_PI,0.5)))*exp(-0.5*x*x);
 }
 
-// This is the Heaviside step function, named after English
-// mathematician Oliver Heaviside. It returns unity when val
-// is greater than or equal to zero and returns zero otherwise
-double heaviside(const double& val) {
-  if (val >= 0) {
-      return 1.0;
-  } else {
-    return 0.0;
-  }
+// An approximation to the cumulative distribution function
+// for the standard normal distribution
+// Note: This is a recursive function
+double norm_cdf(const double& x) {
+    double k = 1.0/(1.0 + 0.2316419*x);
+    double k_sum = k*(0.319381530 + k*(-0.356563782 + k*(1.781477937 + k*(-1.821255978 + 1.330274429*k))));
+
+    if (x >= 0.0) {
+        return (1.0 - (1.0/(pow(2*M_PI,0.5)))*exp(-0.5*x*x) * k_sum);
+    } else {
+        return 1.0 - norm_cdf(-x);
+    }
 }
 
-// Pricing a digital call option with a Monte Carlo method
-double monte_carlo_digital_call_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) {
-  double S_adjust = S * exp(T*(r-0.5*v*v));
-  double S_cur = 0.0;
-  double payoff_sum = 0.0;
-
-  for (int i=0; i<num_sims; i++) {
-    double gauss_bm = gaussian_box_muller();
-    S_cur = S_adjust * exp(sqrt(v*v*T)*gauss_bm);
-    payoff_sum += heaviside(S_cur - K);
-  }
-
-  return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
+// This calculates d_j, for j in {1,2}. This term appears in the closed
+// form solution for the European call or put price
+double d_j(const int& j, const double& S, const double& K, const double& r, const double& v, const double& T) {
+    return (log(S/K) + (r + (pow(-1,j-1))*0.5*v*v)*T)/(v*(pow(T,0.5)));
 }
 
-// Pricing a digital put option with a Monte Carlo method
-double monte_carlo_digital_put_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) {
-  double S_adjust = S * exp(T*(r-0.5*v*v));
-  double S_cur = 0.0;
-  double payoff_sum = 0.0;
+// Calculate the European vanilla call price based on
+// underlying S, strike K, risk-free rate r, volatility of
+// underlying sigma and time to maturity T
+double call_price(const double& S, const double& K, const double& r, const double& v, const double& T) {
+    return S * norm_cdf(d_j(1, S, K, r, v, T))-K*exp(-r*T) * norm_cdf(d_j(2, S, K, r, v, T));
+}
 
-  for (int i=0; i<num_sims; i++) {
-    double gauss_bm = gaussian_box_muller();
-    S_cur = S_adjust * exp(sqrt(v*v*T)*gauss_bm);
-    payoff_sum += heaviside(K - S_cur);
-  }
-
-  return (payoff_sum / static_cast<double>(num_sims)) * exp(-r*T);
+// Calculate the European vanilla put price based on
+// underlying S, strike K, risk-free rate r, volatility of
+// underlying sigma and time to maturity T
+double put_price(const double& S, const double& K, const double& r, const double& v, const double& T) {
+    return -S*norm_cdf(-d_j(1, S, K, r, v, T))+K*exp(-r*T) * norm_cdf(-d_j(2, S, K, r, v, T));
 }
 
 int main(int argc, char **argv) {
-  // First we create the parameter list                                                                               
-  int num_sims = 10000000;   // Number of simulated asset paths                                                       
-  double S = 100.0;  // Option price                                                                                  
-  double K = 100.0;  // Strike price                                                                                  
-  double r = 0.05;   // Risk-free rate (5%)                                                                           
-  double v = 0.2;    // Volatility of the underlying (20%)                                                            
-  double T = 1.0;    // One year until expiry                                                                         
+    // First we create the parameter list
+    double S = 100.0;  // Option price
+    double K = 100.0;  // Strike price
+    double r = 0.05;   // Risk-free rate (5%)
+    double v = 0.2;    // Volatility of the underlying (20%)
+    double T = 1.0;    // One year until expiry
 
-  // Then we calculate the call/put values via Monte Carlo                                                                          
-  double call = monte_carlo_digital_call_price(num_sims, S, K, r, v, T);
-  double put = monte_carlo_digital_put_price(num_sims, S, K, r, v, T);
+    // Then we calculate the call/put values
+    double call = call_price(S, K, r, v, T);
+    double put = put_price(S, K, r, v, T);
 
-  // Finally we output the parameters and prices                                                                      
-  std::cout << "Number of Paths: " << num_sims << std::endl;
-  std::cout << "Underlying:      " << S << std::endl;
-  std::cout << "Strike:          " << K << std::endl;
-  std::cout << "Risk-Free Rate:  " << r << std::endl;
-  std::cout << "Volatility:      " << v << std::endl;
-  std::cout << "Maturity:        " << T << std::endl;
+    // Finally we output the parameters and prices
+    std::cout << "Underlying:      " << S << std::endl;
+    std::cout << "Strike:          " << K << std::endl;
+    std::cout << "Risk-Free Rate:  " << r << std::endl;
+    std::cout << "Volatility:      " << v << std::endl;
+    std::cout << "Maturity:        " << T << std::endl;
 
-  std::cout << "Call Price:      " << call << std::endl;
-  std::cout << "Put Price:       " << put << std::endl;
+    std::cout << "Call Price:      " << call << std::endl;
+    std::cout << "Put Price:       " << put << std::endl;
 
-  return 0;
+    return 0;
 }
